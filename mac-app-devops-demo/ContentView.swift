47becd3ea06af4529d8739b9f7b0689c4ef3f5be
David Kramer <bacongravy@icloud.com>
2020-05-31 12:17:53 -0700

    Add Xcode-generated project files

---
+++ mac-app-devops-demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  mac-app-devops-demo
+//
+//  Created by David Kramer on 5/31/20.
+//  Copyright © 2020 David Kramer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
